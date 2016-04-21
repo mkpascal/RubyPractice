c4be983f3497090e68de2f427bda4b6307ea02cc
Pascal M <mail@mkpascal.net>
2016-04-20 17:03:17 -0700

    distinct naming

--- distinct.rb
+++ distinct.rb
@@ -1,8 +1,8 @@
-def solution_1(a)
+def find_distinct_solution_1(a)
   a.uniq.count
 end
 
-def solution_2(a)
+def find_distinct_solution_2(a)
   dist = []
   a.each { |e| dist << e unless dist.include?(e) }
   dist.count
